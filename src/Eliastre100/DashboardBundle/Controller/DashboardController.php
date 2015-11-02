356c2be14e12602779be2a0ffcb7a6c628ac9cd6
eliastre100 <eliastre100@eliastre100.fr>
2015-11-02 20:50:49 +0100

    Added list of room (basic) TODO : create pages for result && use people in && search

--- src/Eliastre100/DashboardBundle/Controller/DashboardController.php
+++ src/Eliastre100/DashboardBundle/Controller/DashboardController.php
@@ -3,11 +3,14 @@
 namespace Eliastre100\DashboardBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Eliastre100\RoomBundle\Entity\RoomRepository;
 
 class DashboardController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('Eliastre100DashboardBundle:Dashboard:index.html.twig');
+    	$em = $this->getDoctrine()->getManager();
+    	$rooms = $em->getRepository('Eliastre100RoomsBundle:Room')->findall();
+        return $this->render('Eliastre100DashboardBundle:Dashboard:index.html.twig', array("rooms" => $rooms));
     }
 }
