281bad1b3025894c3e00a3e392f11a018ec0b0a7
eliastre100 <eliastre100@eliastre100.fr>
2015-11-08 16:28:27 +0100

    Added some styles

--- src/Eliastre100/RoomsBundle/Controller/DefaultController.php
+++ src/Eliastre100/RoomsBundle/Controller/DefaultController.php
@@ -8,6 +8,14 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+    	 $message = \Swift_Message::newInstance()
+        ->setSubject('Hello Email')
+        ->setFrom('send@example.com')
+        ->setTo('recipient@example.com')
+        ->setBody('You should see me from the profiler!')
+    ;
+
+    $this->get('mailer')->send($message);
         return $this->render('Eliastre100RoomsBundle:Default:index.html.twig', array('name' => $name));
     }
 }
